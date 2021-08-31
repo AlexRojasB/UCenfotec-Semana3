ec58745f8268bd997911b7d0e9ac038d5d7f5af5
Alexander Rojas <alexrrojas.b@gmail.com>
2021-08-30 22:05:43 -0600

    Start third homework

---
+++ Scoreboard-Navigation/Scoreboard-Navigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scoreboard-Navigation
+//
+//  Created by Alexander Rojas Benavides on 8/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
